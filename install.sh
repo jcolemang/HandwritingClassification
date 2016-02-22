0d895ef826cf527dd9b6e8541adcdaf3c79d5720
James Gibson <colemanjones@gmail.com>
2016-02-21 21:29:46 -0500

    Added tornado to the install file

--- install.sh
+++ install.sh
@@ -15,6 +15,9 @@ conda install -c https://conda.binstar.org/tlatorre pygame
 # Install PIL (python image library)
 conda install pillow
 
+# tornado webserver (for the website)
+conda install tornado
+
 # For compiling the C extensions
 sudo apt-get install python-dev
 
